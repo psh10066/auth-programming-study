6cdf621db372dc97310871c135c00bcedb74a244
이호준 <hjlee@kai-i.com>
2023-10-06 16:21:44 +0900

    선생님 - 학생 도메인 설계

---
+++ comp/paper-user/src/main/java/com/sp/fc/user/domain/Authority.java
@@ -0,0 +1,30 @@
+package com.sp.fc.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "sp_authority")
+@IdClass(Authority.class) // 복합키 사용
+public class Authority implements GrantedAuthority {
+
+    public static final String ROLE_TEACHER = "ROLE_TEACHER";
+    public static final String ROLE_STUDENT = "ROLE_STUDENT";
+
+    @Id
+    private Long userId;
+
+    @Id
+    private String authority;
+}
