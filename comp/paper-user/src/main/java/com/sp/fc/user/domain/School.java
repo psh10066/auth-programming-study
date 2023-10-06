af8e16b807bf2fc55fdf6f061acd2cde8e9e4811
이호준 <hjlee@kai-i.com>
2023-10-06 15:06:24 +0900

    학교 도메인 설계

---
+++ comp/paper-user/src/main/java/com/sp/fc/user/domain/School.java
@@ -0,0 +1,30 @@
+package com.sp.fc.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "sp_school")
+public class School {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long schoolId;
+
+    private String name;
+
+    private String city;
+
+    @Column(updatable = false)
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+}
