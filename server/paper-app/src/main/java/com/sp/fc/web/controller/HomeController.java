475917f16faecbb24057c38f68abfb0ac7f0c7d2
이호준 <hjlee@kai-i.com>
2023-10-06 19:23:01 +0900

    학생 - 선생님 - 관리자 사이트 화면 구축

--- server/paper-app/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/paper-app/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.sp.fc.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -11,5 +13,25 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("name", "test");
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam String site, Model model) {
+        model.addAttribute("site", site);
+
+        return "loginForm";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@RequestParam String site, Model model) {
+        model.addAttribute("site", site);
+
+        return "redirect:/" + site;
+    }
+
+    @GetMapping("/signup")
+    public String signUp(@RequestParam String site) {
+
+        return "redirect:/" + site + "/signup";
     }
 }
---
+++ web/site-student/src/main/java/com/sp/fc/site/student/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.sp.fc.site.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/student")
+public class StudentController {
+
+    @GetMapping("")
+    public String index() {
+
+        return "/student/index";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+
+        return "/student/signup";
+    }
+}
